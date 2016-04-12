fd66c25366781e0e693924239a4772590b80579b
Lahiru Dhananjaya <lahirudhananjaya23@gmail.com>
2016-04-12 08:58:38 +0530

    update/delete added

--- server.js
+++ server.js
@@ -37,6 +37,24 @@ app.delete('/contactlist/:id',function(req,res){
 	});
 })
 
+app.get('/contactlist/:id',function(req,res){
+	var id= req.params.id;
+	console.log(id);	
+	db.contactlist.findOne({_id:mongojs.ObjectId(id)},function(err,doc){
+		res.json(doc);
+	});
+});
+
+app.put('/contactlist/:id',function(req,res){
+	var id= req.params.id;
+	console.log(req.body.name);	
+	db.contactlist.findAndModify({query:{_id:mongojs.ObjectId(id)},
+		update:{$set:{name:req.body.name,email:req.body.email,num:req.body.num}},
+		new:true},function(err,doc){
+			res.json(doc);
+	});
+});
+
 app.listen(3000);
 
 console.log("Server running on port 3000");
